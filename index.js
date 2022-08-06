5f5a2366c117f418d064b301e98ecf8baaf1a92c
Natsu <79554925+natsumi-ii@users.noreply.github.com>
2022-08-06 17:12:07 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+let comment;
+SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event) => {
+  comment = event.results[0][0].transcript;
+};
+
+recognition.start();
+const h1 = document.getElementsByName;
+const h2 = document.createElement("h2");
+h2.textContent = comment;
+document.body.appendChild(h1);
+
+const url = "";
+axios
+  .post(url, {
+    player: comment,
+  })
+  .then(function (response) {
+    console.log(response);
+    const h3 = document.createElement("h3");
+    h3.textContent = response;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
