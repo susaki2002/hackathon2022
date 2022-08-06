b66a5756c9e2cf75764549dde9f293046e701bf4
Natsu <79554925+natsumi-ii@users.noreply.github.com>
2022-08-06 23:15:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const axios = require("axios");
+import axios from './axios';
+
 let comment;
+
 SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
 const recognition = new SpeechRecognition();
 
@@ -7,11 +9,14 @@ recognition.onresult = (event) => {
   comment = event.results[0][0].transcript;
 };
 
-recognition.start();
+// function startRecord() {
+  recognition.start();
+  console.log('hello');
+// }
 const h1 = document.getElementsByName;
 const h2 = document.createElement("h2");
 h2.textContent = comment;
-document.body.appendChild(h1);
+// document.body.appendChild(h1);
 
 const url = "./notebook7c3c0a745c.ipynb";
 axios
