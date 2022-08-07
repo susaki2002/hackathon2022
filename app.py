cdc19886da24f740a33679aa728043e205e17529
Itsunori <ittsu120219@gmail.com>
2022-08-07 21:37:45 +0900

    success

--- app.py
+++ app.py
@@ -15,32 +15,32 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
 app = Flask(__name__)
+step=0
 #chat_history_ids=""
 
 # chatting 5 times with nucleus sampling & tweaking temperature
 @app.route('/', methods=['POST', 'GET'])
 def send():
-    comment = json.dumps(request.data.decode('utf-8'))#input(json形式)で'key'がキー
-    input_txt = comment
-    #input_ids = tokenizer.encode(input_txt + tokenizer.eos_token, return_tensors="pt")
+    comment = json.loads(request.data.decode('utf-8'))#input(json形式)で'key'がキー
+    input_txt = comment["key"]
+    input_ids = tokenizer.encode(input_txt + tokenizer.eos_token, return_tensors="pt")
+    bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if step > 0 else input_ids
+    chat_history_ids = model.generate(
+        bot_input_ids,
+        max_length=1000,
+        do_sample=True,
+        top_p=0.98,
+        top_k=30,
+        temperature=0.75,
+        pad_token_id=tokenizer.eos_token_id
+    )
     chat = nlp(transformers.Conversation(input_txt), pad_token_id=50256)
     res = str(chat)
     res = res[res.find("bot >> ")+6:].strip()
     data = {"response":res}
+    step+=1
     return jsonify(data)
 
-# concatenate new user input with chat history (if there is)
-#bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if step > 0 else input_ids
-# generate a bot response
-#chat_history_ids = model.generate(
-    bot_input_ids,
-    max_length=1000,
-    do_sample=True,
-    top_p=0.98,
-    top_k=30,
-    temperature=0.75,
-    pad_token_id=tokenizer.eos_token_id
-#)
 #print(chat_history_ids)
 
 if __name__ == '__main__':
