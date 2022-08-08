5d25b08b1947b78f8db07fa6b4941223c36cbbc9
Itsunori <76028081+Itsunori@users.noreply.github.com>
2022-08-08 11:10:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 import json
 import io
@@ -15,15 +16,18 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
 app = Flask(__name__)
+CORS(app)
+
 step=0
 #chat_history_ids=""
 
 # chatting 5 times with nucleus sampling & tweaking temperature
 @app.route('/', methods=['POST', 'GET'])
 def send():
+    app.logger.debug('DEBUG')
     # input (json format), key is the "key"
     comment = json.loads(request.data.decode('utf-8'))
-    input_txt = comment["key"]
+    input_txt = request.json["key"]
     input_ids = tokenizer.encode(input_txt + tokenizer.eos_token, return_tensors="pt")
     bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if step > 0 else input_ids
     chat_history_ids = model.generate(
